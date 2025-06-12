d1efd50393d0ab6b322ebf61d1357f0661e09e4d
nmartinho <compmedchemlab@users.noreply.github.com>
2025-06-12 09:13:37 +0100

    fix processing chains to account for all scenarios

--- bin/process_chains.py
+++ bin/process_chains.py
@@ -19,7 +19,7 @@ from uniprot_map_api import *
 parser = argparse.ArgumentParser(description='Process all PDBs to annotate all chains with a possible protein identifier.')
 parser.add_argument('--pdbspath', type=str, required=True, dest="pdbpath", help='Path to directory containing all PDBs to process')
 parser.add_argument('--pdbsToRedo', type=str, required=False, nargs='+', default=["None"], dest="pdbs_redo", help='a list of space-separated pdb files to re-run (only used in re-runs of this script)')
-args = parser.parse_args() #                                 nargs='+'
+args = parser.parse_args() #   nargs='+'
 
 pdbpath = args.pdbpath
 pdbs_redo = args.pdbs_redo
@@ -40,7 +40,7 @@ pdbs = [x for x in os.listdir(pdbpath) if x.endswith(".pdb")]
 # old2new
 testprotein_lst = []
 
-for pdbname in pdbs: 
+for pdbname in pdbs:
     pdbcode = pdbname.split(".")[0]
     ciffile = f"cifs/{pdbcode}.cif"
     if glob(ciffile) == 0:
@@ -48,8 +48,14 @@ for pdbname in pdbs:
     data = CifFileReader().read(ciffile)
     data = data[pdbcode.upper()]
     if "_struct_ref_seq" in data:
+        struct_ref = pd.DataFrame.from_dict(data["_struct_ref"], orient="index").T
+        struct_ref = struct_ref[["id", "db_name"]]
+        struct_ref = struct_ref.query("db_name == 'UNP'")
+        struct_ref = struct_ref.rename(columns = {"id":"ref_id"})
         testprotein = pd.DataFrame.from_dict(data["_struct_ref_seq"], orient="index").T
-        testprotein = testprotein[["pdbx_PDB_id_code", "pdbx_db_accession", "pdbx_strand_id"]]
+        # ref_id is a pointer to _struct_ref.id
+        testprotein = testprotein[["ref_id", "pdbx_PDB_id_code", "pdbx_db_accession", "pdbx_strand_id"]]
+        testprotein = testprotein.merge(struct_ref, on="ref_id")
         testprotein_lst.append(testprotein)
 
 testprotein_lst = pd.concat(testprotein_lst)
@@ -69,53 +75,38 @@ new_uniprots = pd.DataFrame(new_uniprots, columns = ["pdbs", "chainName", "unipr
 new_uniprots = new_uniprots[[x.startswith("NOR")==False for x in new_uniprots.uniprot.values]]
 new_uniprots = new_uniprots.drop_duplicates()
 
-# In some instances the pdb has no Uniprot chains annotated - 5fv8
 old2new_lst = []
 chains2solve = []
 for pdb, ch in testprotein_lst[["pdbx_PDB_id_code", "pdbx_strand_id"]].drop_duplicates().values:
-    #if unip.startswith("NOR"): continue
-    res = new_uniprots.query(f"pdbs == '{pdb}' and chainName == '{ch}'").uniprot.values
-    if len(res)==0: continue
-    #res = [x for x in res if x.startswith("NOR")==False]
-    q = testprotein_lst.query(f"pdbx_PDB_id_code == '{pdb}' and pdbx_strand_id == '{ch}'").pdbx_db_accession.values
-    if len(res)>1 and len(q) == len(res):
-        #print("Warning N-to-N mapping", pdb, ch, res)
-        if np.isin(q,res).all(): 
-            old2new = pd.DataFrame([res,q]).T
-            #print(old2new)
-        else:
-            for q_i in q: chains2solve.append([pdb,ch, q_i])
-            sys.exit(pdb)
-    #new_uniprots.query(f"pdbs == '{pdb}' and chainName =='{ch}'").index
-    #elif len(q) == 0:# No uniprots to solve
-    elif len(q) == len(res) and len(q) == 1:
-        #OK
-        old2new = pd.DataFrame([res,q]).T
-    # there are cases where one of the chains is not shown in pdb_chain_uniprot.csv.gz
-    # looks like it might be when the chain is a peptide (2f1y)
-    elif len(q) > len(res) and np.isin(res, q).all():
-        # some chains were dropped
-        old2new = pd.DataFrame([res,res]).T
-    elif len(q) > len(res) and np.isin(res, q).all() == False:
-        # some chains dropped and some chains are not directly mapped in SIFTS
-        # This might be risky to assign chains to the list; just use chains in SIFTS
-        old2new = pd.DataFrame([res,res]).T
-    else:
-        print(f"WARNING: chain mapping between SIFTS and the CIF file is strange in {pdb}, please inspect and add manually to *_process_uniprot_chains.txt if needed")
-        old2new = pd.DataFrame(["",""]).T
-    old2new.loc[:,"pdb"] = pdb
-    old2new.loc[:,"chain"] = ch
-    old2new_lst.append(old2new)
+    res = new_uniprots.query(f"pdbs == '{pdb}' and chainName == '{ch}'").drop_duplicates()[['chainName', 'uniprot']]
+    #comes from the raw cif file
+    q = testprotein_lst.query(f"pdbx_PDB_id_code == '{pdb}' and pdbx_strand_id == '{ch}'").drop_duplicates()[['pdbx_db_accession', 'pdbx_strand_id']]
+    if len(res) == 0:
+        # there is nothing to do as there are no uniprot codes for this chain in the SIFTS file
+        q.loc[:,"pdb"] = pdb
+        q.loc[:,"uniprot"] = q.pdbx_db_accession.values
+        q = q[['uniprot', "pdbx_db_accession", 'pdbx_strand_id', "pdb"]]
+        q=q.rename(columns={"pdbx_strand_id":"chainName"})
+        old2new_lst.append(q)
+        continue
+    if len(q) > 1:
+        # More than one uniprot is mapped to a chain, currently in the file. We cannot assert old-2-new dict
+        # probably a chimera
+        for q_i in q.pdbx_db_accession.unique(): chains2solve.append([pdb,ch, q_i])
+        continue
 
+    uniprot_pairs = res.merge(q, right_on="pdbx_strand_id", left_on="chainName").drop_duplicates()[['uniprot', "pdbx_db_accession", 'chainName']]
+    uniprot_pairs.loc[:,"pdb"] = pdb
+    uniprot_pairs = uniprot_pairs[['uniprot', "pdbx_db_accession", 'chainName', "pdb"]]
+    old2new_lst.append(uniprot_pairs)
 
 old2new_lst = pd.concat(old2new_lst)
-old2new_lst.columns = ["updated_uniprot", "uniprot_in_pdbfile", "pdb", "chain"]
+old2new_lst.columns = ["updated_uniprot", "uniprot_in_pdbfile", "chain", "pdb"]
 old2new_lst = old2new_lst.drop_duplicates()
 old2new_lst = old2new_lst[[ "pdb", "chain", "uniprot_in_pdbfile", "updated_uniprot"]]
-old2new_lst = old2new_lst.query("uniprot_in_pdbfile != ''")
+
 
 if len(chains2solve)>0:
-    
     uniprots2solve = np.unique([x[-1] for x in chains2solve])
     job_id = submit_id_mapping(
         from_db="UniProtKB_AC-ID", to_db="UniProtKB", ids=list(uniprots2solve)
